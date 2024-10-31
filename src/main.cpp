8693a9774abda99b005f946ff17262d2ce452182
yingjie_wang <2778809626@qq.com>
2024-10-31 17:27:21 +0800

    重构

--- src/main.cpp
+++ src/main.cpp
@@ -2,59 +2,32 @@
 #include "socket/socket.h"
 
 #include <filesystem>
-#include<iostream>
+#include <iostream>
 
 
-
-
-
-
-
-
-int main() 
+int main()
 {
-
-    std::string ip = "192.168.0.162"; // 服务器IP地址
-    int port = 8080; // 服务器端口号
+    std::string ip = "192.168.0.162";  // 服务器IP地址
+    int port = 8080;                   // 服务器端口号
 
     ////if (Socket::tcpClientSync(ip, port))  // 同步
-    //if (so.tcpClientAsyn(ip, port))  // 异步
+    // if (so.tcpClientAsyn(ip, port))  // 异步
     //{
-    //    std::cout << "TCP client executed successfully" << std::endl;
-    //}
-    //else 
+    //     std::cout << "TCP client executed successfully" << std::endl;
+    // }
+    // else
     //{
-    //    std::cout << "TCP client failed" << std::endl;
-    //}
+    //     std::cout << "TCP client failed" << std::endl;
+    // }
 
     if (Socket::updClientSync(ip, port))  // 同步
     {
         std::cout << "TCP client executed successfully" << std::endl;
     }
-    else {
+    else
+    {
         std::cout << "TCP client failed" << std::endl;
     }
 
     return 0;
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
